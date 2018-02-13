a451e2afcddca084e6c3274fb917dd72c4d09260
Dominic Tölle <dominic.tolle@gmail.com>
2018-02-13 11:31:28 +0100

    Add registration field validation

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -22,13 +22,13 @@ export class RegisterComponent implements OnInit {
   constructor(
     private userService: UserService,
     private alertService: AlertService,
-    private router: Router) {}
+    private router: Router) { }
 
   ngOnInit() {
     console.log('RegisterComponent ngOnInit() called');
     this.loadInstitutions();
     this.registerForm = new FormGroup({
-      institution: new FormControl('0', Validators.required),
+      institution: new FormControl(null, Validators.required),
       firstName: new FormControl(null, Validators.required),
       lastName: new FormControl(null, Validators.required),
       email: new FormControl(null, [
@@ -36,8 +36,8 @@ export class RegisterComponent implements OnInit {
         Validators.email
       ]),
       password1: new FormControl(null, Validators.required),
-      password2: new FormControl(null, Validators.required),
-    });
+      password2: new FormControl(null),
+    }, this.passwordConfirmationValidator);
   }
 
   getInstitutionName(institution: Institution): string {
@@ -49,47 +49,66 @@ export class RegisterComponent implements OnInit {
     return name;
   }
 
-  loadInstitutions() {
+  private loadInstitutions() {
     this.userService.getAllInstitutions()
-    .subscribe((data) => {
-      for (const entry of data as Array<any>) {
-        const institution = new Institution(entry);
-        // console.log('institution: ', institution);
-        this.institutions.push(institution);
-      }
-    }, (err: HttpErrorResponse) => {
-      try {
-        const errObj = JSON.parse(err.error);
-        console.log('error loadInstitutions: ', errObj);
-        this.alertService.error(errObj.title);
-      } catch (e) {}
-    });
+      .subscribe((data) => {
+        for (const entry of data as Array<any>) {
+          const institution = new Institution(entry);
+          // console.log('institution: ', institution);
+          this.institutions.push(institution);
+        }
+      }, (err: HttpErrorResponse) => {
+        try {
+          const errObj = JSON.parse(err.error);
+          console.log('error loadInstitutions: ', errObj);
+          this.alertService.error(errObj.title);
+        } catch (e) { }
+      });
   }
 
   register() {
-    this.loading = true;
+    if (this.registerForm.valid) {
+      this.loading = true;
 
-    console.log('this.registerForm.value.institution: ', this.registerForm.value.institution);
+      console.log('this.registerForm.value.institution: ', this.registerForm.value.institution);
 
-    const user = new User(
-      this.registerForm.value.email,
-      this.registerForm.value.password1,
-      this.registerForm.value.firstName,
-      this.registerForm.value.lastName,
-    );
-    user.institution = this.registerForm.value.institution;
+      const user = new User(
+        this.registerForm.value.email,
+        this.registerForm.value.password1,
+        this.registerForm.value.firstName,
+        this.registerForm.value.lastName,
+      );
+      user.institution = this.registerForm.value.institution;
 
-    this.userService.create(user)
-      .subscribe((data) => {
-        this.alertService.success(data['title'], true);
-        this.router.navigate(['users/login']);
-      }, (err: HttpErrorResponse) => {
-        const errObj = JSON.parse(err.error);
-        this.alertService.error(errObj.title, true);
-        this.loading = false;
-      });
+      this.userService.create(user)
+        .subscribe((data) => {
+          this.alertService.success(data['title'], true);
+          this.router.navigate(['users/login']);
+        }, (err: HttpErrorResponse) => {
+          const errObj = JSON.parse(err.error);
+          this.alertService.error(errObj.title, true);
+          this.loading = false;
+        });
 
-    this.registerForm.reset();
+      this.registerForm.reset();
+    }
   }
 
+  validateField(fieldName: string) {
+    return this.registerForm.controls[fieldName].valid
+      || this.registerForm.controls[fieldName].untouched
+  }
+
+  private passwordConfirmationValidator(fg: FormGroup) {
+    let pw1 = fg.controls.password1;
+    let pw2 = fg.controls.password2;
+
+    if (pw1.value !== pw2.value) {
+      pw2.setErrors({ validatePasswordConfirm: true });
+    }
+    else {
+      pw2.setErrors(null);
+    }
+    return null;
+  }
 }
