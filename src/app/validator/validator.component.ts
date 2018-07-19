5c1788831c295a6f4aaa75b413fac0173b0f6974
Birgit Lewicki <lewicki.birgit@gmail.com>
2018-07-19 19:03:06 +0200

    Improve sending data when no new warnings.
    Ticket: #82

--- src/app/validator/validator.component.ts
+++ src/app/validator/validator.component.ts
@@ -22,10 +22,16 @@ import { JsonToExcelService, IBlobData } from '../services/json-to-excel.service
 import { AuthService } from '../auth/services/auth.service';
 
 
+interface IWarning {
+  row: number;
+  col: number;
+}
+
 interface IErrorColors {
   red: boolean;
   yellow: boolean;
   blue: boolean;
+  warnings: IWarning[];
 }
 
 
@@ -52,7 +58,13 @@ export class ValidatorComponent implements OnInit, OnDestroy {
   subscriptions = [];
   private message: string;
   private dataChanged: boolean = false;
-  private currentErrors: IErrorColors;
+  private currentErrors: IErrorColors = {
+    red: false,
+    yellow: false,
+    blue: false,
+    warnings: []
+  };
+  private newWarnings: boolean = false;
 
   constructor(private uploadService: UploadService,
               private validateService: ValidateService,
@@ -82,6 +94,7 @@ export class ValidatorComponent implements OnInit, OnDestroy {
   }
 
   async initializeTable() {
+    this.newWarnings = false;
     this.tableStructureProvider = this.uploadService.getCurrentTableStructureProvider();
     if (this.tableStructureProvider) {
       this.tableData = this.tableStructureProvider.getTableData();
@@ -229,10 +242,13 @@ export class ValidatorComponent implements OnInit, OnDestroy {
   }
 
   getErrorColors(): IErrorColors {
+    const prevWarnings: IWarning[] = this.currentErrors.warnings;
+    let curWarnings: IWarning[] = [];
     let errorColors: IErrorColors = {
       red: false,
       yellow: false,
-      blue: false
+      blue: false,
+      warnings: []
     }
 
     _.forEach((this.errData), (rowItem, rowIndex) => {
@@ -242,9 +258,18 @@ export class ValidatorComponent implements OnInit, OnDestroy {
         }
         if (colItem[1]) {
           errorColors.yellow = true;
+          const curWarning: IWarning = {
+            row: rowIndex,
+            col: colIndex
+          }
+          curWarnings.push(curWarning);
+          if (_.findIndex(prevWarnings, (prevWarning) =>  _.isMatch(prevWarning, curWarning)) === -1) {
+            this.newWarnings = true;
+          }
         }
       });
     });
+    errorColors.warnings = curWarnings;
 
     return errorColors;
   }
@@ -253,60 +278,69 @@ export class ValidatorComponent implements OnInit, OnDestroy {
     let blobData: IBlobData = await this.saveAsExcel(false);
 
     try {
-      let formData: FormData = new FormData();
-      formData.append('myMemoryXSLX', blobData.blob, blobData.fileName);
       if (this.currentErrors.red) {
         this.message = 'Es gibt noch rot gekennzeichnete Fehler. Bitte vor dem Senden korrigieren.';
         this.alertService.error(this.message, false);
       } else if ((this.currentErrors.yellow) && (this.dataChanged)) {
-        this.message = 'Sie haben nach dem letzten Validierungsvorgang Änderungen gemacht, die zu neuen Ergebnissen führen. Wenn Sie dennoch senden wollen, dann klicken Sie bitte nochmals Senden.';
-        this.alertService.error(this.message, false);
-        this.dataChanged = false;
-    } else {
-        await this.alertService.clear();
-
-        const options: ConfirmSettings = {
-          overlay: true,
-          overlayClickToClose: false,
-          showCloseButton: true,
-          confirmText: 'Ok',
-          declineText: 'Cancel'
+        if (!this.newWarnings) {
+          this.sendData(blobData);
+        } else {
+          this.message = 'Sie haben nach dem letzten Validierungsvorgang Änderungen gemacht, die zu neuen Ergebnissen führen. Wenn Sie dennoch senden wollen, dann klicken Sie bitte nochmals Senden.';
+          this.alertService.error(this.message, false);
+          this.dataChanged = false;
         }
-
-        this.confirmationService.create('Senden', `<p>Ihre Probendaten werden jetzt an das BfR gesendet.</p>
-                                                   <p>Bitte vergessen Sie nicht die Exceltabelle in Ihrem Mailanhang
-                                                   auszudrucken und Ihren Isolaten beizulegen.</p>`, options)
-          .subscribe((ans: ResolveEmit) => {
-            if (ans.resolved) {
-              const currentUser = this.authService.getCurrentUser();
-              formData.append('firstName', currentUser.firstName);
-              formData.append('lastName', currentUser.lastName);
-              formData.append('email', currentUser.email);
-              formData.append('institution', currentUser.institution.name1);
-              formData.append('location', currentUser.institution.name2);
-              this.validateService.sendFile(formData)
-                .subscribe((event: HttpEvent<Event>) => {
-                  if (event instanceof HttpResponse) {
-                    this.message = `Der Auftrag wurde an das BfR gesendet.
-                                    Bitte drucken Sie die Exceltabelle in Ihrem Mailanhang
-                                    aus und legen sie Ihren Isolaten bei.`;
-                    this.alertService.success(this.message, false);
-                  }
-                }, (err: HttpErrorResponse) => {
-                  const errMessage = err['error']['error'];
-                  this.message = errMessage;
-                  this.alertService.error(errMessage, false);
-                });
-              } else {
-              this.message = 'Es wurden keine Probendaten an das BfR gesendet';
-              this.alertService.error(this.message, false);
-            };
-          });
+      } else {
+        this.sendData(blobData);
       };
     } catch (err) {
       this.message = 'Problem beim Speichern der validierten Daten als Excel';
       this.alertService.error(this.message, false);
     }
+  }
+
+  async sendData(blobData: IBlobData) {
+    let formData: FormData = new FormData();
+    formData.append('myMemoryXSLX', blobData.blob, blobData.fileName);
+
+    await this.alertService.clear();
+
+    const options: ConfirmSettings = {
+      overlay: true,
+      overlayClickToClose: false,
+      showCloseButton: true,
+      confirmText: 'Ok',
+      declineText: 'Cancel'
+    }
+
+    this.confirmationService.create('Senden', `<p>Ihre Probendaten werden jetzt an das BfR gesendet.</p>
+                                               <p>Bitte vergessen Sie nicht die Exceltabelle in Ihrem Mailanhang
+                                               auszudrucken und Ihren Isolaten beizulegen.</p>`, options)
+      .subscribe((ans: ResolveEmit) => {
+        if (ans.resolved) {
+          const currentUser = this.authService.getCurrentUser();
+          formData.append('firstName', currentUser.firstName);
+          formData.append('lastName', currentUser.lastName);
+          formData.append('email', currentUser.email);
+          formData.append('institution', currentUser.institution.name1);
+          formData.append('location', currentUser.institution.name2);
+          this.validateService.sendFile(formData)
+            .subscribe((event: HttpEvent<Event>) => {
+              if (event instanceof HttpResponse) {
+                this.message = `Der Auftrag wurde an das BfR gesendet.
+                                Bitte drucken Sie die Exceltabelle in Ihrem Mailanhang
+                                aus und legen sie Ihren Isolaten bei.`;
+                this.alertService.success(this.message, false);
+              }
+            }, (err: HttpErrorResponse) => {
+              const errMessage = err['error']['error'];
+              this.message = errMessage;
+              this.alertService.error(errMessage, false);
+            });
+          } else {
+          this.message = 'Es wurden keine Probendaten an das BfR gesendet';
+          this.alertService.error(this.message, false);
+        };
+      });
   }
 
   setCurrentJsResponseDTO(responseDTO: IJsResponseDTO[]) {
