dadfc6dfa15bff86d54dc86b17c019e934f14195
Dominic Tölle <dominic.tolle@gmail.com>
2018-10-23 13:20:30 +0200

    Add highlight of changes to downloaded Excel
    Ticket: #129

--- src/app/samples/services/excel-converter.service.ts
+++ src/app/samples/services/excel-converter.service.ts
@@ -6,8 +6,9 @@ import * as XlsxPopulate from 'xlsx-populate/browser/xlsx-populate';
 import { WindowRefService } from './window-ref.service';
 import {
     ISampleSheet,
-    IImportedExcelFileDetails, SampleData, VALID_SHEET_NAME, FORM_PROPERTIES, IExcelFileBlob
+    IImportedExcelFileDetails, SampleData, VALID_SHEET_NAME, FORM_PROPERTIES, IExcelFileBlob, ChangedValueCollection
 } from '../model/sample-management.model';
+import { ClientError } from '../../core/model/client-error';
 
 // TODO: Actionize
 @Injectable({
@@ -24,7 +25,7 @@ export class ExcelConverterService {
     async convertToExcel(data: ISampleSheet, fileNameAddon: string = ''): Promise<IExcelFileBlob> {
 
         if (!data.workSheet) {
-            throw new Error('No Excel data available.');
+            throw new ClientError('No Excel data available.');
         }
         const file: File = data.workSheet.file;
         const oriFileName = file.name;
@@ -36,9 +37,10 @@ export class ExcelConverterService {
 
         fileName += fileNameAddon + '.xlsx';
 
+        const highlights = data.formData.map(e => e.edits);
         const dataToSave = this.fromDataObjToAOO(data.formData.map(e => e.data));
         let workbook = await this.fromFileToWorkbook(file);
-        workbook = this.addValidatedDataToWorkbook(data.workSheet, workbook, dataToSave);
+        workbook = this.addValidatedDataToWorkbook(data.workSheet, workbook, dataToSave, highlights);
 
         const blob = await workbook.outputAsync();
 
@@ -70,13 +72,16 @@ export class ExcelConverterService {
         return dataToSave;
     }
 
-    private addValidatedDataToWorkbook(originalWorkSheet: IImportedExcelFileDetails, workbook: any, dataToSave: any) {
+    private addValidatedDataToWorkbook(
+        originalWorkSheet: IImportedExcelFileDetails, workbook: any, dataToSave: any, highlights: ChangedValueCollection[] = []) {
 
         const oriDataLength = originalWorkSheet.oriDataLength;
         const searchTerm = 'Ihre Probe-';
         const sheet = workbook.sheet(VALID_SHEET_NAME);
 
         if (sheet) {
+            const startCol = 'A';
+            const endCol = String.fromCharCode(startCol.charCodeAt(0) + FORM_PROPERTIES.length);
             const result = sheet.find(searchTerm);
             if (result.length > 0) {
                 const cell = result[0];
@@ -89,10 +94,39 @@ export class ExcelConverterService {
                     }
                 }
 
-                const startCell = 'A' + (rowNumber + 1);
+                const startRow = (rowNumber + 1);
+                const startCell = startCol + startRow;
                 sheet.cell(startCell).value(dataToSave);
+                try {
+                    const endCell = endCol + (startRow + dataToSave.length);
+                    const rng = sheet.range(startCell + ':' + endCell);
+                    rng.style({ fill: 'ffffff' });
+                    this.highlightEdits(sheet, highlights, startCol, startRow);
+
+                } catch (e) {
+                    throw new ClientError('Unable to apply styling to Excel');
+                }
+
             }
         }
         return workbook;
+    }
+
+    private highlightEdits(sheet: any, highlights: ChangedValueCollection[], startCol: string, startRow: number) {
+        _.forEach(highlights,
+            (row, index) => {
+                if (!_.isEmpty(row)) {
+                    _.forEach(row, (v, k) => {
+                        const col = _.findIndex(FORM_PROPERTIES, e => e === k);
+                        if (col !== -1) {
+                            const changedCol = String.fromCharCode(startCol.charCodeAt(0) + col);
+                            sheet.cell(changedCol + (startRow + index)).style({ fill: '0de5cf' });
+                        }
+
+                    });
+
+                }
+            }
+        );
     }
 }
