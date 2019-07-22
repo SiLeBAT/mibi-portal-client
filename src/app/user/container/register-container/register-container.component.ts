9808ac8357418368af284c29ee7e6356dc369276
Isaak Gerber <isaak.gerber@bfr.bund.de>
2019-07-22 17:57:12 +0200

    User is not allowed to register with a non listed institute.
    Now institut selection list pops up with first click.
    
    Ticket: #234
    Change-Id: I3c3e25a7c7b531f08bb7ca9c9f5a8f45d96f691f

--- src/app/user/container/register-container/register-container.component.ts
+++ src/app/user/container/register-container/register-container.component.ts
@@ -8,7 +8,7 @@ import { UserActionService } from '../../../core/services/user-action.service';
 import { UserActionType } from '../../../shared/model/user-action.model';
 import { RegistrationDetails, UserRegistrationRequest } from '../../model/user.model';
 import { Observable } from 'rxjs/internal/Observable';
-import { takeWhile, map } from 'rxjs/operators';
+import { takeWhile, map, filter } from 'rxjs/operators';
 import { ClientError } from '../../../core/model/client-error';
 import { selectInstitutions } from '../../state/user.reducer';
 import { selectSupportContact, ContentMainStates } from '../../../content/state/content.reducer';
@@ -17,7 +17,11 @@ import { ContentSlice } from '../../../content/content.state';
 
 @Component({
     selector: 'mibi-register-container',
-    template: `<mibi-register (register)="register($event)" [institutions]="institutions$ | async"></mibi-register>`
+    template: `<mibi-register *ngIf="(institutions$ | async) as institutions"
+        (register)="register($event)"
+        [institutions]="institutions"
+        [supportContact]="supportContact">
+        </mibi-register>`
 })
 export class RegisterContainerComponent implements OnInit, OnDestroy {
 
@@ -91,11 +95,11 @@ export class RegisterContainerComponent implements OnInit, OnDestroy {
     private loadInstitutions() {
         this.institutions$ = this.store.pipe(
             select(selectInstitutions),
+            filter((value) => value.length > 0),
             map((data: InstitutionDTO[]) => {
                 return data.map(institution => {
                     return fromDTOToInstitution(institution);
                 });
-
             }));
     }
 }
