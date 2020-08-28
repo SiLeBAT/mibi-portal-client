df0d33e205a840ae5d8a8a17217000016f855044
Isaak Gerber <isaak.gerber@bfr.bund.de>
2020-08-28 18:16:57 +0200

    Update "Probeneinsendebogen" to v15
    - Removed sample sheet version hint from home page
    - Moved sample sheet template url from inline code to environment variable
    
    Ticket: #405
    Change-Id: I075f1f2894b044fc2e952f0130c8b02e52c9c819

--- src/environments/default.environment.ts
+++ src/environments/default.environment.ts
@@ -3,5 +3,6 @@ export const environment = {
     appName: 'MiBi-Portal',
     supportContact: 'mibi-portal@bfr.bund.de',
     version: require('../../package.json').version,
-    lastChange: require('../../package.json').mibiConfig.lastChange
+    lastChange: require('../../package.json').mibiConfig.lastChange,
+    sampleSheetURL: 'https://www.bfr.bund.de/cm/343/Einsendebogen-v14-1.xlsx'
 };
