565586a40486294344bb791d70ed0ceb2e139902
birgitlewicki <lewicki.birgit@gmail.com>
2023-12-20 23:37:34 +0100

    Fix linting

--- src/app/core/services/user-action.service.ts
+++ src/app/core/services/user-action.service.ts
@@ -4,7 +4,6 @@ import { UserActionViewModelConfiguration, UserActionType } from '../../shared/m
 import { Store } from '@ngrx/store';
 import { validateSamplesSSA } from '../../samples/validate-samples/validate-samples.actions';
 import { CoreMainSlice } from '../core.state';
-import { environment } from '../../../environments/environment';
 import { closeSamplesSSA } from '../../samples/close-samples/close-samples.actions';
 import { importSamplesMSA } from '../../samples/import-samples/import-samples.actions';
 import { exportSamplesSSA } from '../../samples/export-samples/export-samples.actions';
