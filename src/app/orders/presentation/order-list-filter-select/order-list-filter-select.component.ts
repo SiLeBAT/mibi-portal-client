056105be648a67343886fee4ecb893d681e33a24
mahtabiltarabian <m.iltarabian68@gmail.com>
2026-07-09 15:59:35 +0200

    Inform users about new BfR results - order list results filter
    Ticket:https://github.com/SiLeBAT/mibi-portal-client/issues/784

---
+++ src/app/orders/presentation/order-list-filter-select/order-list-filter-select.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+export type ResultsFilterValue = '' | 'partial' | 'complete';
+
+@Component({
+    standalone: false,
+    selector: 'mibi-order-list-filter-select',
+    templateUrl: './order-list-filter-select.component.html',
+    styleUrls: ['./order-list-filter-select.component.scss']
+})
+export class OrderListFilterSelectComponent {
+    @Input() value: ResultsFilterValue = '';
+    @Input() ariaLabel = '';
+    @Output() valueChange = new EventEmitter<ResultsFilterValue>();
+
+    onSelect(next: ResultsFilterValue): void {
+        this.valueChange.emit(next);
+    }
+
+    onClear(): void {
+        this.valueChange.emit('');
+    }
+}
